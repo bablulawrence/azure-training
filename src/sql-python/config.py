f34bfd498e563ed32e7b6c28f5b225cddc06c166
Bablu Lawrence <35357232+bablulawrence@users.noreply.github.com>
2021-11-24 09:57:25 +0530

    Delete src directory

--- src/sql-python/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://cosmosdb-sql-779.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'pVJQhrTJv0SQeQNWNhP2IoxbztPIPXAYVM2N4tCMbzzJFx1fzjv6gGbO24FulV2QVMZrd0y4aMZSgq2bop832A=='),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'ToDoList'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
-}
