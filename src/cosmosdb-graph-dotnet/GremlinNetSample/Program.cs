16130a70821836fba15070f734fcd00235a1d65e
Bablu Lawrence <Bablu.Lawrence@Unilever.com>
2021-11-24 10:01:22 +0530

    fixed issues source code samples

---
+++ src/cosmosdb-graph-dotnet/GremlinNetSample/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+using Gremlin.Net.Driver;
+using Gremlin.Net.Driver.Exceptions;
+using Gremlin.Net.Structure.IO.GraphSON;
+using Newtonsoft.Json;
+
+namespace GremlinNetSample
+{
+    /// <summary>
+    /// Sample program that shows how to get started with the Graph (Gremlin) APIs for Azure Cosmos DB using the open-source connector Gremlin.Net
+    /// </summary>
+    class Program
+    {
+        // Azure Cosmos DB Configuration variables
+        // Replace the values in these variables to your own.
+        // <configureConnectivity>
+        // private static string Host => Environment.GetEnvironmentVariable("Host") ?? throw new ArgumentException("Missing env var: Host");
+        // private static string PrimaryKey => Environment.GetEnvironmentVariable("PrimaryKey") ?? throw new ArgumentException("Missing env var: PrimaryKey");
+        // private static string Database => Environment.GetEnvironmentVariable("DatabaseName") ?? throw new ArgumentException("Missing env var: DatabaseName");
+        // private static string Container => Environment.GetEnvironmentVariable("ContainerName") ?? throw new ArgumentException("Missing env var: ContainerName");
+
+        private static string Host = "cosmosdb-gremlin-779.gremlin.cosmosdb.azure.com";
+        private static string PrimaryKey = "UwXyNp2IChVc1NaTGy1lA5vG96z6q86kb6okmai5VbeaWdAfWnaQ0JA0FCBT46Hvm30IEpdJUx8pDUHhgX0b5g==";
+        private static string Database = "sample-database";
+        private static string Container = "sample-graph";
+        private static bool EnableSSL
+        {
+            get
+            {
+                if (Environment.GetEnvironmentVariable("EnableSSL") == null)
+                {
+                    return true;
+                }
+
+                if (!bool.TryParse(Environment.GetEnvironmentVariable("EnableSSL"), out bool value))
+                {
+                    throw new ArgumentException("Invalid env var: EnableSSL is not a boolean");
+                }
+
+                return value;
+            }
+        }
+
+        private static int Port
+        {
+            get
+            {
+                if (Environment.GetEnvironmentVariable("Port") == null)
+                {
+                    return 443;
+                }
+
+                if (!int.TryParse(Environment.GetEnvironmentVariable("Port"), out int port))
+                {
+                    throw new ArgumentException("Invalid env var: Port is not an integer");
+                }
+
+                return port;
+            }
+        }
+
+        // </configureConnectivity>
+
+        // Gremlin queries that will be executed.
+        // <defineQueries>
+        private static Dictionary<string, string> gremlinQueries = new Dictionary<string, string>
+        {
+            { "Cleanup",        "g.V().drop()" },
+            { "AddVertex 1",    "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44).property('pk', 'pk')" },
+            { "AddVertex 2",    "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39).property('pk', 'pk')" },
+            { "AddVertex 3",    "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller').property('pk', 'pk')" },
+            { "AddVertex 4",    "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield').property('pk', 'pk')" },
+            { "AddEdge 1",      "g.V('thomas').addE('knows').to(g.V('mary'))" },
+            { "AddEdge 2",      "g.V('thomas').addE('knows').to(g.V('ben'))" },
+            { "AddEdge 3",      "g.V('ben').addE('knows').to(g.V('robin'))" },
+            { "UpdateVertex",   "g.V('thomas').property('age', 44)" },
+            { "CountVertices",  "g.V().count()" },
+            { "Filter Range",   "g.V().hasLabel('person').has('age', gt(40))" },
+            { "Project",        "g.V().hasLabel('person').values('firstName')" },
+            { "Sort",           "g.V().hasLabel('person').order().by('firstName', decr)" },
+            { "Traverse",       "g.V('thomas').out('knows').hasLabel('person')" },
+            { "Traverse 2x",    "g.V('thomas').out('knows').hasLabel('person').out('knows').hasLabel('person')" },
+            { "Loop",           "g.V('thomas').repeat(out()).until(has('id', 'robin')).path()" },
+            { "DropEdge",       "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()" },
+            { "CountEdges",     "g.E().count()" },
+            { "DropVertex",     "g.V('thomas').drop()" },
+        };
+        // </defineQueries>
+
+        // Starts a console application that executes every Gremlin query in the gremlinQueries dictionary. 
+        static void Main(string[] args)
+        {
+            // <defineClientandServerObjects>
+            string containerLink = "/dbs/" + Database + "/colls/" + Container;
+            Console.WriteLine($"Connecting to: host: {Host}, port: {Port}, container: {containerLink}, ssl: {EnableSSL}");
+            var gremlinServer = new GremlinServer(Host, Port, enableSsl: EnableSSL,
+                                                    username: containerLink,
+                                                    password: PrimaryKey);
+
+            ConnectionPoolSettings connectionPoolSettings = new ConnectionPoolSettings()
+            {
+                MaxInProcessPerConnection = 10,
+                PoolSize = 30,
+                ReconnectionAttempts = 3,
+                ReconnectionBaseDelay = TimeSpan.FromMilliseconds(500)
+            };
+
+            var webSocketConfiguration =
+                new Action<ClientWebSocketOptions>(options =>
+                {
+                    options.KeepAliveInterval = TimeSpan.FromSeconds(10);
+                });
+
+
+            using (var gremlinClient = new GremlinClient(
+                gremlinServer,
+                new GraphSON2Reader(),
+                new GraphSON2Writer(),
+                GremlinClient.GraphSON2MimeType,
+                connectionPoolSettings,
+                webSocketConfiguration))
+            {
+                // </defineClientandServerObjects>
+
+                // <executeQueries>
+                foreach (var query in gremlinQueries)
+                {
+                    Console.WriteLine(String.Format("Running this query: {0}: {1}", query.Key, query.Value));
+
+                    // Create async task to execute the Gremlin query.
+                    var resultSet = SubmitRequest(gremlinClient, query).Result;
+                    if (resultSet.Count > 0)
+                    {
+                        Console.WriteLine("\tResult:");
+                        foreach (var result in resultSet)
+                        {
+                            // The vertex results are formed as Dictionaries with a nested dictionary for their properties
+                            string output = JsonConvert.SerializeObject(result);
+                            Console.WriteLine($"\t{output}");
+                        }
+                        Console.WriteLine();
+                    }
+
+                    // Print the status attributes for the result set.
+                    // This includes the following:
+                    //  x-ms-status-code            : This is the sub-status code which is specific to Cosmos DB.
+                    //  x-ms-total-request-charge   : The total request units charged for processing a request.
+                    //  x-ms-total-server-time-ms   : The total time executing processing the request on the server.
+                    PrintStatusAttributes(resultSet.StatusAttributes);
+                    Console.WriteLine();
+                }
+                // </executeQueries>
+            }
+
+            // Exit program
+            Console.WriteLine("Done. Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        private static Task<ResultSet<dynamic>> SubmitRequest(GremlinClient gremlinClient, KeyValuePair<string, string> query)
+        {
+            try
+            {
+                return gremlinClient.SubmitAsync<dynamic>(query.Value);
+            }
+            catch (ResponseException e)
+            {
+                Console.WriteLine("\tRequest Error!");
+
+                // Print the Gremlin status code.
+                Console.WriteLine($"\tStatusCode: {e.StatusCode}");
+
+                // On error, ResponseException.StatusAttributes will include the common StatusAttributes for successful requests, as well as
+                // additional attributes for retry handling and diagnostics.
+                // These include:
+                //  x-ms-retry-after-ms         : The number of milliseconds to wait to retry the operation after an initial operation was throttled. This will be populated when
+                //                              : attribute 'x-ms-status-code' returns 429.
+                //  x-ms-activity-id            : Represents a unique identifier for the operation. Commonly used for troubleshooting purposes.
+                PrintStatusAttributes(e.StatusAttributes);
+                Console.WriteLine($"\t[\"x-ms-retry-after-ms\"] : { GetValueAsString(e.StatusAttributes, "x-ms-retry-after-ms")}");
+                Console.WriteLine($"\t[\"x-ms-activity-id\"] : { GetValueAsString(e.StatusAttributes, "x-ms-activity-id")}");
+
+                throw;
+            }
+        }
+
+        private static void PrintStatusAttributes(IReadOnlyDictionary<string, object> attributes)
+        {
+            Console.WriteLine($"\tStatusAttributes:");
+            Console.WriteLine($"\t[\"x-ms-status-code\"] : { GetValueAsString(attributes, "x-ms-status-code")}");
+            Console.WriteLine($"\t[\"x-ms-total-server-time-ms\"] : { GetValueAsString(attributes, "x-ms-total-server-time-ms")}");
+            Console.WriteLine($"\t[\"x-ms-total-request-charge\"] : { GetValueAsString(attributes, "x-ms-total-request-charge")}");
+        }
+
+        public static string GetValueAsString(IReadOnlyDictionary<string, object> dictionary, string key)
+        {
+            return JsonConvert.SerializeObject(GetValueOrDefault(dictionary, key));
+        }
+
+        public static object GetValueOrDefault(IReadOnlyDictionary<string, object> dictionary, string key)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                return dictionary[key];
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/cosmosdb-graph-dotnet/README.md
@@ -0,0 +1,82 @@
+---
+page_type: sample
+languages:
+- csharp
+products:
+- azure
+description: "Azure Cosmos DB is a globally distributed, multi-model database for mission critical applications."
+urlFragment: azure-cosmos-db-graph-gremlindotnet-getting-started
+---
+
+# Getting started with Azure Cosmos DB: Graph API
+Azure Cosmos DB is a globally distributed, multi-model database for mission critical applications. Azure Cosmos DB provides the Graph API for applications that need to model, query, and traverse large graphs efficiently using the Gremlin standard.
+
+## About this sample: Using the Gremlin.Net open-source connector with Cosmos DB and its advantages.
+
+This sample uses the open-source [Gremlin.Net driver](https://github.com/apache/tinkerpop/tree/master/gremlin-dotnet) to connect to an Azure Cosmos DB Graph API account and run some basic Create, Read, Update, Delete Gremlin queries. 
+
+## Getting Started
+
+### Prerequisites
+
+The only dependency is the [Gremlin.Net driver](https://www.nuget.org/packages/Gremlin.Net/3.4.6) version `3.4.6`, which you can install with the following instructions:
+
+- Using .NET CLI:
+
+    ```
+    dotnet add package Gremlin.Net
+    ```
+
+- Using Powershell Package Manager:
+
+    ```
+    Install-Package Gremlin.Net -Version 3.4.6
+    ```
+
+- For *.NET CORE* use the `nuget` [command-line utility](https://docs.microsoft.com/en-us/nuget/install-nuget-client-tools):
+
+    ```
+    nuget install Gremlin.Net
+    ```
+
+## Input your database identifiers
+
+The program will take the database connection parameters from the environment variables. You'll need to specify the following variables:
+- `Host`: The endpoint to your Gremlin database. This has the following format `xxxx.gremlin.cosmos.azure.com`
+- `PrimaryKey`: Your unique access key to your database. This is a long, secure combination of characters and numbers.
+- `DatabaseName`: The name of your database that groups your containers.
+- `ContainerName`: The name of the container, also known as graph, where your data is direclty stored.
+
+You can set an environment variable like this: 
+- Powershell: `$env:YourVariable="value"`
+- CMD: `setx YourVariable="value"`
+
+## Code
+The following dictionary, under `Program.cs`, includes all the Gremlin queries that will be executed serially:
+```cs
+Dictionary<string, string> gremlinQueries = new Dictionary<string, string>
+{
+    { "Cleanup",        "g.V().drop()" },
+    { "AddVertex 1",    "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44)" },
+    { "AddVertex 2",    "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39)" },
+    { "AddVertex 3",    "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller')" },
+    { "AddVertex 4",    "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield')" },
+    { "AddEdge 1",      "g.V('thomas').addE('knows').to(g.V('mary'))" },
+    { "AddEdge 2",      "g.V('thomas').addE('knows').to(g.V('ben'))" },
+    { "AddEdge 3",      "g.V('ben').addE('knows').to(g.V('robin'))" },
+    { "UpdateVertex",   "g.V('thomas').property('age', 44)" },
+    { "CountVertices",  "g.V().count()" },
+    { "Filter Range",   "g.V().hasLabel('person').has('age', gt(40))" },
+    { "Project",        "g.V().hasLabel('person').values('firstName')" },
+    { "Sort",           "g.V().hasLabel('person').order().by('firstName', decr)" },
+    { "Traverse",       "g.V('thomas').outE('knows').inV().hasLabel('person')" },
+    { "Traverse 2x",    "g.V('thomas').outE('knows').inV().hasLabel('person').outE('knows').inV().hasLabel('person')" },
+    { "Loop",           "g.V('thomas').repeat(out()).until(has('id', 'robin')).path()" },
+    { "DropEdge",       "g.V('thomas').outE('knows').where(inV().has('id', 'mary')).drop()" },
+    { "CountEdges",     "g.E().count()" },
+    { "DropVertex",     "g.V('thomas').drop()" },
+};
+```
+
+## Graph database support overview
+[Azure Cosmos DB](http://cosmosdb.com) provides you with a fully-managed graph database service with global distribution, elastic scaling of storage and throughput, automatic indexing and query, tunable consistency levels, and supports the Gremlin standard. It also provides the ability to use multiple models like document and graph over the same data. For example, you can use a document collection to store graph data side by side with documents, and use both SQL queries over JSON and Gremlin queries to query the collection. For Graph API, this documentation page lists all the supported Gremlin steps: [Azure Cosmos DB Gremlin Graph Support](https://docs.microsoft.com/en-us/azure/cosmos-db/gremlin-support).
